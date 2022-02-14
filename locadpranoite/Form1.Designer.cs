942b1b5935d69659e3db33b880e26f2a8699cfc9
Laboratórios - W10 - Fatec Mauá <labs@labtec.local>
2022-02-14 19:40:53 -0300

    add

--- locadpranoite/Form1.Designer.cs
+++ locadpranoite/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpasswd = new System.Windows.Forms.TextBox();
+            this.btnenter = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(140, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuário:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(140, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha:";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuser.Location = new System.Drawing.Point(332, 82);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(100, 26);
+            this.txtuser.TabIndex = 2;
+            // 
+            // txtpasswd
+            // 
+            this.txtpasswd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpasswd.Location = new System.Drawing.Point(332, 207);
+            this.txtpasswd.Name = "txtpasswd";
+            this.txtpasswd.Size = new System.Drawing.Size(100, 26);
+            this.txtpasswd.TabIndex = 3;
+            // 
+            // btnenter
+            // 
+            this.btnenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenter.Location = new System.Drawing.Point(123, 336);
+            this.btnenter.Name = "btnenter";
+            this.btnenter.Size = new System.Drawing.Size(101, 41);
+            this.btnenter.TabIndex = 4;
+            this.btnenter.Text = "Entrar";
+            this.btnenter.UseVisualStyleBackColor = true;
+            // 
+            // btncancel
+            // 
+            this.btncancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncancel.Location = new System.Drawing.Point(331, 336);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(101, 41);
+            this.btncancel.TabIndex = 5;
+            this.btncancel.Text = "Cancelar";
+            this.btncancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(576, 450);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnenter);
+            this.Controls.Add(this.txtpasswd);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpasswd;
+        private System.Windows.Forms.Button btnenter;
+        private System.Windows.Forms.Button btncancel;
     }
 }
 
---
+++ locadpranoite/Form2.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace locadpranoite
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpesquisa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnpesquisa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtpesquisa
+            // 
+            this.txtpesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpesquisa.Location = new System.Drawing.Point(185, 55);
+            this.txtpesquisa.Name = "txtpesquisa";
+            this.txtpesquisa.Size = new System.Drawing.Size(174, 26);
+            this.txtpesquisa.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Usuário:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(47, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(490, 259);
+            this.panel1.TabIndex = 9;
+            // 
+            // btnpesquisa
+            // 
+            this.btnpesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpesquisa.Location = new System.Drawing.Point(405, 58);
+            this.btnpesquisa.Name = "btnpesquisa";
+            this.btnpesquisa.Size = new System.Drawing.Size(97, 26);
+            this.btnpesquisa.TabIndex = 10;
+            this.btnpesquisa.Text = "Pesquisar";
+            this.btnpesquisa.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 409);
+            this.Controls.Add(this.btnpesquisa);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtpesquisa);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpesquisa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnpesquisa;
+    }
+}
