403cd2b21d8322f631a2cb3e133fef9be5c1bcad
Laboratórios - W10 - Fatec Mauá <labs@labtec.local>
2022-02-14 20:11:50 -0300

    add

--- locadpranoite/Form2.Designer.cs
+++ locadpranoite/Form2.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtpesquisa = new System.Windows.Forms.TextBox();
+            this.RR = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pnlpesquisa = new System.Windows.Forms.Panel();
             this.btnpesquisa = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // txtpesquisa
+            // RR
             // 
-            this.txtpesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtpesquisa.Location = new System.Drawing.Point(185, 55);
-            this.txtpesquisa.Name = "txtpesquisa";
-            this.txtpesquisa.Size = new System.Drawing.Size(174, 26);
-            this.txtpesquisa.TabIndex = 8;
+            this.RR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RR.Location = new System.Drawing.Point(185, 55);
+            this.RR.Name = "RR";
+            this.RR.Size = new System.Drawing.Size(174, 26);
+            this.RR.TabIndex = 8;
             // 
             // label1
             // 
@@ -69,6 +69,7 @@
             this.btnpesquisa.TabIndex = 10;
             this.btnpesquisa.Text = "Pesquisar";
             this.btnpesquisa.UseVisualStyleBackColor = true;
+            this.btnpesquisa.Click += new System.EventHandler(this.Btnpesquisa_Click);
             // 
             // Form2
             // 
@@ -77,10 +78,11 @@
             this.ClientSize = new System.Drawing.Size(584, 409);
             this.Controls.Add(this.btnpesquisa);
             this.Controls.Add(this.pnlpesquisa);
-            this.Controls.Add(this.txtpesquisa);
+            this.Controls.Add(this.RR);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,7 +90,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtpesquisa;
+        private System.Windows.Forms.TextBox RR;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel pnlpesquisa;
         private System.Windows.Forms.Button btnpesquisa;
--- locadpranoite/Form2.cs
+++ locadpranoite/Form2.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
 
 namespace locadpranoite
 {
     public partial class Form2 : Form
     {
+
+        private SqlConnection conn;
+        private SqlDataAdapter adapter;
+        private DataTable tblveiculos;
+        private string strsql, strconn;
+
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Btnpesquisa_Click(object sender, EventArgs e)
+        {
+            strconn = "data source=(local); initial catalog = locadora; integrated security=sspi";
+            conn = new SqlConnection(strconn);
+            conn.Open();
+
+            tblveiculos = new DataTable();
+
+            b = "select * from veiculos";
+            adapter = new SqlDataAdapter(b, conn);
+            adapter.Fill(tblveiculos);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
